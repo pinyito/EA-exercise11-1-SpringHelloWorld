e8559d589cea47b16b70c35b43e74d4d06603ca0
Eria Othieno Pinyi <pinyito@users.noreply.github.com>
2022-05-03 16:08:25 -0500

    Created the springconfig.xml file along with the necessary application classes.

---
+++ exercise11-1/src/main/java/HelloApp.java
@@ -0,0 +1,10 @@
+import org.springframework.context.ConfigurableApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+public class HelloApp {
+    public static void main(String[] args){
+        ConfigurableApplicationContext context = new ClassPathXmlApplicationContext("springconfig.xml");
+        Greeting greetingService = context.getBean("greetingService", Greeting.class);
+        greetingService.sayHello();
+    }
+}
